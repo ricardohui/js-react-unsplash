a6f231d6972d4c6a001617f7ec1e5e93a099d412
Ricardo <eehaha@gmail.com>
2019-01-23 22:36:34 +0800

    Finished for React

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { spans: 0 };
     this.imageRef = React.createRef();
   }
 
@@ -17,7 +18,7 @@ class ImageCard extends React.Component {
   render() {
     const { description, src } = this.props;
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} alt={description} src={src} />
       </div>
     );
